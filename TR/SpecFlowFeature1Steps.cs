6a56cba63949dc05bbf09d91da395edd71dc475e
Aya Metwalli <metwalliaya@gmail.com>
2021-01-20 21:44:09 +0100

    Add project files.

---
+++ TR/SpecFlowFeature1Steps.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace TR
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        int f, s, u;
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int p0)
+        {
+            f = p0;
+        }
+        
+        [Given(@"the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int p0)
+        {
+            s = p0;
+        }
+        
+        [When(@"the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            u = f + s;
+         
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            var actual = u;
+            var expected = p0;
+            Assert.Equal(actual, expected);
+        }
+    }
+}
